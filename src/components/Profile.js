dd123cf5c20d5ec675794aa5cfe37811b403e0e9
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-05-09 01:59:59 -0500

    Links added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,10 @@ import Image from 'react-bootstrap/Image'
 import photo from '../profile.jpg';
 import Card from 'react-bootstrap/Card'
 import '../App.css';
+
 import { Link } from 'react-router-dom';
+import {Route, Switch} from "react-router";
+
 import { IconContext } from "react-icons";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
@@ -21,10 +24,10 @@ const Profile = ({size}) => {
         <Card.Subtitle className="mb-2 text-muted">MS in Data Science & MA in Economics</Card.Subtitle>
         <Card.Text className="text-center">
         <div>
-        <FaGithub style={style} className="h_space"/>
-        <FaTwitter style={style} className="h_space"/>
-        <FaLinkedin style={style} className="h_space"/>
-        <SiGmail style={style} className="h_space"/>
+        <a href="https://github.com/C1587S"><FaGithub style={style} className="h_space"/></a>
+        <a href="https://twitter.com/cadavid_sanchez"><FaTwitter style={style} className="h_space"/></a>
+        <a href="https://www.linkedin.com/in/s-cadavid-sanchez/"><FaLinkedin style={style} className="h_space"/></a>
+        <a href="mailto: s.cadavid1587@gmail.com"><SiGmail style={style} className="h_space"/></a>
         </div>
         </Card.Text>
     </Card.Body>

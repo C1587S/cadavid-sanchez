ad9b791bbd455d0f30f05bb38b73b0bfe0fd8b3c
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-05-09 01:15:13 -0500

    Include contact icons

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,6 +4,8 @@ import Image from 'react-bootstrap/Image'
 import photo from '../profile.jpg';
 import Card from 'react-bootstrap/Card'
 
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
 const Profile = ({size}) => {
     return(
     <Card style={{ width: '20rem' }} className="mt-1">
@@ -13,6 +15,9 @@ const Profile = ({size}) => {
         <Card.Title>Sebastián Cadavid-Sánchez</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">MS in Data Science & MA in Economics</Card.Subtitle>
         <Card.Text className="text-center">
+        <FaLinkedin />
+        <FaGithub />
+        <FaTwitter />
         </Card.Text>
     </Card.Body>
     </Card>

42781ed40f1c9f7755f68f1885a3f34104d0aa1b
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-05-10 23:57:17 -0500

    Fixes Research dropdown menu

--- src/components/NavBarHeader.js
+++ src/components/NavBarHeader.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import CV from "../pdfs/SCS_CV.pdf"
 
 const NavMenu = () => {
     return(
-        <Navbar className="mt-2" collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar className="mt-2" collapseOnSelect expand="sm" bg="dark" variant="dark">
         <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav>
-            </Nav>
-            <Nav>
+            <Nav class="navbar navbar-dark bg-dark variant-dark">
             <Nav.Link href={CV} target = "_blank">CV</Nav.Link>
-            <Nav.Link href="#research">Research</Nav.Link>
             </Nav>
+            <NavDropdown class="navbar navbar-dark bg-dark variant-dark" title="Research" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#economics">Economics</NavDropdown.Item>
+            <NavDropdown.Item href="#datascience">Data Science</NavDropdown.Item>
+            </NavDropdown>
         </Navbar.Collapse>
         </Navbar>
+        
 
     )
 }
--- src/components/Research.js
+++ src/components/Research.js
@@ -13,9 +13,9 @@ function Research() {
       <div><h1 class="colored_heading_h1">Research</h1></div>
       <br />
       <Row className="justify-content-center">
-      <div><h3 class="colored_heading_h3">Data Science</h3></div>
+      <div><h3 id="datascience" class="colored_heading_h3">Data Science</h3></div>
       <DataScienceResearch />
-      <div><h3 class="colored_heading_h3">Economics</h3></div>
+      <div><h3 id="economics" class="colored_heading_h3">Economics</h3></div>
       <EconomicsResearch />
       </Row>
     </Container>

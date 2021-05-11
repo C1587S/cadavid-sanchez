cdd608543eee7626ca6ec805f1f4ac8e3d49a5a8
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-05-10 20:02:10 -0500

    Adds images for com18

--- src/components/Economics/EconomicsResearch.js
+++ src/components/Economics/EconomicsResearch.js
@@ -22,6 +22,7 @@ function EconomicsResearch() {
     <ul className="list-unstyled">
       <Media as="li">
       <div>
+      <br />
         <img
           width={120}
           height={190}
@@ -66,6 +67,7 @@ function EconomicsResearch() {
 
       <Media as="li">
         <div>
+        <br />
         <img
           width={120}
           height={190}
@@ -109,44 +111,8 @@ function EconomicsResearch() {
       </Media>
 
       <Media as="li">
-        {/* <div>
-        <img
-          width={120}
-          height={120}
-          className="mr-3"
-          src={}
-          alt="Generic placeholder"
-        />
-        <br />
-        <img
-          width={120}
-          height={120}
-          className="mr-3"
-          src={}
-          alt="Generic placeholder"
-        />
-        </div> */}
-
-        <Media.Body>
-          <br />
-          <a href="https://albertoortiznet.files.wordpress.com/2018/03/monetary_and_fiscal_policies_interactions_in_mexico_1981_-_2016___march022018.pdf">  
-          <h5 class="colored_heading_h5">Monetary and Fiscal Policy Interactions in Mexico, 1981-2016</h5>
-          </a>
-          <p className="p_ind">
-          This paper analyzes the role of fiscal and monetary policies in the determination of inflation and government debt 
-      in Mexico during the 1981-2016 period. A Markov-switching DSGE model estimation allows us to identify five different 
-      periods of fiscal and monetary policy interactions congruent with a historical account of the Mexican monetary and 
-      fiscal policy mix during the past 35 years. Counterfactual exercises show that the low-frequency evolution of inflation 
-      is mainly determined by the monetary policy stance, while the fiscal policy stance mainly determines the low-frequency 
-      evolution of debt. We show that if monetary dominance prevailed throughout the whole period, the average inflation would 
-      have been 13.2% rather than the 20.4% observed. On the other hand, complete fiscal dominance would have implied average 
-      inflation of 42% and an average debt five times larger than the observed.
-          </p>
-        </Media.Body>
-      </Media>
-
-      <Media as="li">
       <div>
+        <br />
         <img
           width={120}
           height={190}
@@ -170,27 +136,65 @@ function EconomicsResearch() {
           </a>
           <p className="p_ind">
           Decreases (increases) in long-term interest rates caused by compressions (dilations) of term premiums could be 
-      financially expansive (contractive) and might require monetary policy restraints (stimulus). This paper uses 
-      measures of the term premium calculated by Adrian et al. (2013) to perform Bayesian estimations of a Markov-switching 
-      vector autoregression (MS-VAR) model as Hubrich and Tetlow (2015), finding evidence of the importance of allowing 
-      for switching parameters (nonlinearities) and switching variance (non-Gaussian) when analyzing macrofinancial linkages 
-      in the United States. Using the specification with the best fit to the data of two Markov states for parameters and 
-      three Markov states for variances, we estimate a Markov-switching dynamic stochastic general equilibrium (MS-DSGE) 
-      macroeconomic model with financial frictions in long-term debt instruments developed by Carlstrom et al. (2017) to 
-      provide evidence on how financial conditions have evolved in the us since 1962 and how the Federal Reserve Bank has 
-      responded to the evolution of term premiums. 
-      <br></br><br></br>
-      Using the estimated model, we perform a counterfactual analysis of the 
-      potential evolution of macroeconomic and financial variables under alternative financial conditions and monetary policy 
-      responses. We analyze six episodes with the presence of high financial frictions and/or medium and high shocks volatility. 
-      In three of them there was a high monetary policy response to financial factors: 1978Q4-1983Q4 which helped to mitigate 
-      inflation at the cost of economic activity, and the 1990Q2-1993Q4 and 2010Q1-2011Q4 episodes in which the high response 
-      served to mitigate economic contractions. Meanwhile, in the three episodes where low response to financial factors is 
-      observed, if the monetary authority had responded more aggressively, from 1971Q1-1978Q3 it could have attained lower 
-      inflation at the cost of lower GDP, from 2000Q4-2004Q4 it could have delayed the GDP contraction to 2002Q3, but this 
-      would have been deeper and inflation larger, and in 2006Q1-2009Q4 it might have precipitated the GDP contraction. 
-      The presence of high financial frictions and high shock volatility makes recessions deeper and recoveries more sluggish 
-      showing the importance of the financial-macroeconomic nexus.
+          financially expansive (contractive) and might require monetary policy restraints (stimulus). This paper uses 
+          measures of the term premium calculated by Adrian et al. (2013) to perform Bayesian estimations of a Markov-switching 
+          vector autoregression (MS-VAR) model as Hubrich and Tetlow (2015), finding evidence of the importance of allowing 
+          for switching parameters (nonlinearities) and switching variance (non-Gaussian) when analyzing macrofinancial linkages 
+          in the United States. Using the specification with the best fit to the data of two Markov states for parameters and 
+          three Markov states for variances, we estimate a Markov-switching dynamic stochastic general equilibrium (MS-DSGE) 
+          macroeconomic model with financial frictions in long-term debt instruments developed by Carlstrom et al. (2017) to 
+          provide evidence on how financial conditions have evolved in the us since 1962 and how the Federal Reserve Bank has 
+          responded to the evolution of term premiums. 
+          <br></br><br></br>
+          Using the estimated model, we perform a counterfactual analysis of the 
+          potential evolution of macroeconomic and financial variables under alternative financial conditions and monetary policy 
+          responses. We analyze six episodes with the presence of high financial frictions and/or medium and high shocks volatility. 
+          In three of them there was a high monetary policy response to financial factors: 1978Q4-1983Q4 which helped to mitigate 
+          inflation at the cost of economic activity, and the 1990Q2-1993Q4 and 2010Q1-2011Q4 episodes in which the high response 
+          served to mitigate economic contractions. Meanwhile, in the three episodes where low response to financial factors is 
+          observed, if the monetary authority had responded more aggressively, from 1971Q1-1978Q3 it could have attained lower 
+          inflation at the cost of lower GDP, from 2000Q4-2004Q4 it could have delayed the GDP contraction to 2002Q3, but this 
+          would have been deeper and inflation larger, and in 2006Q1-2009Q4 it might have precipitated the GDP contraction. 
+          The presence of high financial frictions and high shock volatility makes recessions deeper and recoveries more sluggish 
+          showing the importance of the financial-macroeconomic nexus.
+          </p>
+        </Media.Body>
+      </Media>
+
+      <Media as="li">
+        <div>
+        <br />
+        <img
+          width={140}
+          height={140}
+          className="mr-3"
+          src={com18_img_cfs}
+          alt="Generic placeholder"
+        />
+        {/* <br />
+        <img
+          width={120}
+          height={120}
+          className="mr-3"
+          src={com18_img_cfs}
+          alt="Generic placeholder"
+        /> */}
+        </div>
+
+        <Media.Body>
+          <br />
+          <a href="https://albertoortiznet.files.wordpress.com/2018/03/monetary_and_fiscal_policies_interactions_in_mexico_1981_-_2016___march022018.pdf">  
+          <h5 class="colored_heading_h5">Monetary and Fiscal Policy Interactions in Mexico, 1981-2016</h5>
+          </a>
+          <p className="p_ind">
+          This paper analyzes the role of fiscal and monetary policies in the determination of inflation and government debt 
+          in Mexico during the 1981-2016 period. A Markov-switching DSGE model estimation allows us to identify five different 
+          periods of fiscal and monetary policy interactions congruent with a historical account of the Mexican monetary and 
+          fiscal policy mix during the past 35 years. Counterfactual exercises show that the low-frequency evolution of inflation 
+          is mainly determined by the monetary policy stance, while the fiscal policy stance mainly determines the low-frequency 
+          evolution of debt. We show that if monetary dominance prevailed throughout the whole period, the average inflation would 
+          have been 13.2% rather than the 20.4% observed. On the other hand, complete fiscal dominance would have implied average 
+          inflation of 42% and an average debt five times larger than the observed.
           </p>
         </Media.Body>
       </Media>

de5f11d098ee0f8bbdf10d9144b5ef0095f5d68b
Sebastian Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-05-14 12:27:27 -0500

    Updates

---
+++ precache-manifest.22922e3077a0ed906e3fc91cef41e6a5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "40a34493c917e1b2c28a",
+    "url": "/scs-webpage/static/css/main.dc417995.chunk.css"
+  },
+  {
+    "revision": "40a34493c917e1b2c28a",
+    "url": "/scs-webpage/static/js/main.6410e039.chunk.js"
+  },
+  {
+    "revision": "9729d9d65af153e484aa",
+    "url": "/scs-webpage/static/js/runtime~main.b9d27b0d.js"
+  },
+  {
+    "revision": "11157e8a2f3a8c63f07d",
+    "url": "/scs-webpage/static/css/2.db1dd59d.chunk.css"
+  },
+  {
+    "revision": "11157e8a2f3a8c63f07d",
+    "url": "/scs-webpage/static/js/2.f673a654.chunk.js"
+  },
+  {
+    "revision": "4c7a19e055998208c1c3",
+    "url": "/scs-webpage/static/js/3.43ac7eac.chunk.js"
+  },
+  {
+    "revision": "d3a3d7bd01194662ab9ab493d9d4d9d3",
+    "url": "/scs-webpage/static/media/mini_school_markets_3.d3a3d7bd.png"
+  },
+  {
+    "revision": "f8f4469e39a593c7818f2d792471b905",
+    "url": "/scs-webpage/static/media/itam20_mods.f8f4469e.png"
+  },
+  {
+    "revision": "6ad827c26ebac3883cb22e0e278f8926",
+    "url": "/scs-webpage/static/media/itam20_rec.6ad827c2.png"
+  },
+  {
+    "revision": "5c732525cefd682da54189f99c2eec27",
+    "url": "/scs-webpage/static/media/co20_img_front.5c732525.png"
+  },
+  {
+    "revision": "96dfc3a9af4b2df7a25104085456b274",
+    "url": "/scs-webpage/static/media/co20_img_int.96dfc3a9.png"
+  },
+  {
+    "revision": "4a089da080fc7910e4386d23ae3c5e14",
+    "url": "/scs-webpage/static/media/cs18_img_front_alt.4a089da0.png"
+  },
+  {
+    "revision": "31c3fa010df0ac72dccbf475fc65ac64",
+    "url": "/scs-webpage/static/media/cs18_img_irfs.31c3fa01.png"
+  },
+  {
+    "revision": "7358eb0a0d197acfdf3af1a29f87890f",
+    "url": "/scs-webpage/static/media/com18_img_cfs.7358eb0a.png"
+  },
+  {
+    "revision": "71228a394c50e0b2671f26d21e9d4230",
+    "url": "/scs-webpage/static/media/cor18_img_front.71228a39.png"
+  },
+  {
+    "revision": "402ca3bae765708fd9774ca2ab317d74",
+    "url": "/scs-webpage/static/media/cor18_img_srs.402ca3ba.png"
+  },
+  {
+    "revision": "6115e66072d1896d7b041af265a683fe",
+    "url": "/scs-webpage/static/media/SCS_CV.6115e660.pdf"
+  },
+  {
+    "revision": "2a22c2f251d34192aa0a743ad66dc521",
+    "url": "/scs-webpage/static/media/profile.2a22c2f2.jpg"
+  },
+  {
+    "revision": "378e93e54c4a162b604f1326d3a3ad33",
+    "url": "/scs-webpage/index.html"
+  }
+];

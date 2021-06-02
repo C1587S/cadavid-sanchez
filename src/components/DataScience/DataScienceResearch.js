c47654a83d5da5009cc319b13e10cc65ab9a5cb8
S. Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-06-01 20:17:35 -0500

    Updates Schooling Markets summary

--- src/components/DataScience/DataScienceResearch.js
+++ src/components/DataScience/DataScienceResearch.js
@@ -22,11 +22,8 @@ function DataScienceResearch() {
           <h5 class="colored_heading_h5">Schooling Markets in Mexico</h5>
           </a>
           <p className="p_ind">
-          Research  assistance  to  generate  school  markets  to  understand  the  migration  mechanisms  
-          of  stu-dents  among  institutions.   Responsabilities  included:  constructing  andvalidating  
-          master  databasecontaining geographical information about nearly 230,000 Mexican schools With this 
-          database, we generated spatial geometry analysis to construct commuting zones. Using graph algorithms,  
-          we detected communities by utilizing bidirectional migration data of more than one million students.
+          We proposed a methodology for identifying commuting zones and schooling markets in Mexico using geospatial analysis and graph algorithms for community detection. Public policy programs could use school markets to target interventions towards enhancing education quality through school competition mechanisms. 
+          This work was done with Mauricio Romero <a href="https://www.itam.mx/">(Instituto Tecnológico Autónomo de México) </a> and Paola Mejía <a href="https://www.epfl.ch/en/">(EPFL)</a>, at the Center for Economic Research <a href="http://cie.itam.mx/">(CIE) </a>.
           </p>
         </Media.Body>
       </Media>

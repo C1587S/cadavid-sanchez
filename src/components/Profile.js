fa733ac4cc4341b17e4a06b07480ec47fc8d961a
S. Cadavid-Sanchez <s.cadavid1587@gmail.com>
2021-06-01 21:29:27 -0500

    Changes ordering

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -18,8 +18,8 @@ const Profile = ({size}) => {
         <Card.Text className="text-center">
         <div>
         <a href="https://github.com/C1587S"><FaGithub style={style} className="h_space"/></a>
-        <a href="https://twitter.com/cadavid_sanchez"><FaTwitter style={style} className="h_space"/></a>
         <a href="https://www.linkedin.com/in/s-cadavid-sanchez/"><FaLinkedin style={style} className="h_space"/></a>
+        <a href="https://twitter.com/cadavid_sanchez"><FaTwitter style={style} className="h_space"/></a>
         <a href="mailto: s.cadavid1587@gmail.com"><SiGmail style={style} className="h_space"/></a>
         </div>
         </Card.Text>
